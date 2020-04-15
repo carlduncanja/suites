73366a1cb9c2c7881c40f6eb3806f16552a3653c
Akinyele <akinyele.thompson@smsja.net>
2020-04-15 12:06:22 -0500

    -- created level indicator component.

--- src/components/common/LevelIndicator/LevelIndicator.js
+++ src/components/common/LevelIndicator/LevelIndicator.js
@@ -1,79 +1,116 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {View, StyleSheet} from "react-native";
+import {View, StyleSheet, Text} from "react-native";
 
 
 const backgroundColor = "#4E5664";
 const idealColor = "#48BB78";
 const idealColor2 = "#ECC94B";
-const critical = "#ED8936";
+const criticalColor = "#ED8936";
 
 
-function LevelIndicator(props) {
+/**
+ *
+ * @param max {number}K
+ * @param min {number}
+ * @param ideal {number}LK
+ * @param critical {number}
+ * @param level {number}
+ * @returns {*}
+ * @constructor
+ */
+function LevelIndicator({max, min, ideal, critical, level}) {
+    const calculateProgress = (max, level) => {
+        const progress = level / max * 100;
+        return Math.floor(progress)
+    };
 
-    const {
-        max,
-        min,
-        ideal,
-        critical,
-        level
-    } = props;
+    const determineColor = (level) => {
+        if (level >= ideal) return idealColor;
+        else if (level < ideal && level > critical) return idealColor2;
+        else if (level <= critical) return criticalColor;
+        else if (!ideal && level > max * .5) return idealColor;
+        else if (!critical && level < max * .5) return criticalColor;
+        else return idealColor2
+    };
 
-
-    const progress = '100%';
-    const color = idealColor;
-
+    const calculatedProgress = calculateProgress(max, level);
+    const progress = `${calculatedProgress}%`;
+    const color = determineColor(level);
 
     return (
         <View style={styles.container}>
 
-            {/*  PROGRESS  */}
-            <View style={{
-                backgroundColor: color,
-                height: '100%',
-                width: progress
-            }}/>
+            <Text style={styles.label}>{min}</Text>
 
-            {/*  DIVIDERS  */}
-            <View style={styles.dividerContainer}>
-                <View style={styles.divider}/>
-                <View style={styles.divider}/>
-                <View style={styles.divider}/>
-                <View style={styles.divider}/>
+            <View style={styles.progressContainer}>
+                <View style={{flex: 1}}>
+                    {/*  DIVIDERS  */}
+                    <View style={styles.dividerContainer}>
+                        <View style={[styles.divider, {}]}/>
+                        <View style={[styles.divider, {}]}/>
+                        <View style={[styles.divider, {}]}/>
+                        <View style={[styles.divider, {}]}/>
+                    </View>
+
+                    {/*  PROGRESS  */}
+                    <View style={{
+                        position: 'absolute',
+                        height: '100%',
+                        backgroundColor: color,
+                        width: progress
+                    }}/>
+                </View>
             </View>
+
+            <Text style={styles.label}>{max}</Text>
 
         </View>
     );
 }
 
-LevelIndicator.propTypes = {};
-LevelIndicator.defaultProps = {};
-
 const styles = StyleSheet.create({
     container: {
         display: 'flex',
-        // flex: 1,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    progressContainer: {
         width: 60,
         height: 4,
         borderRadius: 4,
+        marginLeft: 10,
+        marginRight: 10,
         backgroundColor,
         overflow: 'hidden'
     },
     dividerContainer: {
-        position: 'absolute',
-        display: 'flex',
         flex: 1,
-        // width: '100%',
-        // height: '100%',
-        // justifyContent: 'center',
-        backgroundColor: "red"
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        zIndex: 10,
     },
     divider: {
         height: "100%",
         width: 2,
         backgroundColor: "#E3E8EF"
+    },
+    label: {
+        color: "#718096",
+        fontSize: 12,
+        fontWeight: "500",
     }
 });
 
+
+LevelIndicator.propTypes = {
+    max: PropTypes.number.isRequired,
+    min: PropTypes.number.isRequired,
+    level: PropTypes.number.isRequired,
+    ideal: PropTypes.number,
+    critical: PropTypes.number,
+};
+LevelIndicator.defaultProps = {};
 
 export default LevelIndicator;
