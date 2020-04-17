ec6f919f45947d2a6ae754bb2d0fc77f978f945e
Akinyele <akinyele.thompson@smsja.net>
2020-04-17 00:25:21 -0500

    -- fixed view hierarchy for BottomSheetModal.js

--- src/components/navigation/SuiteNavigator.js
+++ src/components/navigation/SuiteNavigator.js
@@ -1,12 +1,12 @@
-import React,{useContext} from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, ScrollView, SectionList } from 'react-native';
-import { getList } from '../../hooks/useListHook';
+import React, {useContext} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet, ScrollView, SectionList} from 'react-native';
+import {getList} from '../../hooks/useListHook';
 import NavigationTab from '../SideBar/SideBarTabComponent';
-import { SuitesContext } from '../../contexts/SuitesContext';
-import { appActions } from '../../redux/reducers/suitesAppReducer';
+import {SuitesContext} from '../../contexts/SuitesContext';
+import {appActions} from '../../redux/reducers/suitesAppReducer';
 import SvgIcon from '../../../assets/SvgIcon';
 import {ModalProvider, createModalStack} from 'react-native-modalfy';
-import { createNavigator, TabRouter } from 'react-navigation';
+import {createNavigator, TabRouter} from 'react-navigation';
 import SideBarComponent from "../SideBar/SideBarComponent";
 
 import OverlaySlidePanelModal from '../../modals/OverlaySlidePanelModal';
@@ -26,7 +26,7 @@ import BottomSheetModal from '../../modals/BottomSheetModal';
  */
 export const SuiteNavigator = ({screenDimensions, navigation, descriptors}) => {
 
-    const { routes, index } = navigation.state;
+    const {routes, index} = navigation.state;
     const descriptor = descriptors[routes[index].key];
 
     const ActiveScreen = descriptor.getComponent();
@@ -37,21 +37,23 @@ export const SuiteNavigator = ({screenDimensions, navigation, descriptors}) => {
         OverlayModal: OverlayModal,
         ActionContainerModal: ActionContainerModal,
         ReportPreviewModal: ReportPreviewModal,
-        OverlayInfoModal : OverlayInfoModal,
-        BottomSheetModal : BottomSheetModal,
-    }
+        OverlayInfoModal: OverlayInfoModal,
+        BottomSheetModal: BottomSheetModal,
+    };
 
     const defaultOptions = {
         backdropOpacity: 0,
         position: 'bottom',
         containerStyle: {
             flex: 1,
+            // ...StyleSheet.absoluteFillObject,
             alignItems: 'flex-end',
         }
-    }
-    const stack = createModalStack(modalConfig, defaultOptions)
+    };
 
-    const [state,dispatch] = useContext(SuitesContext)
+    const stack = createModalStack(modalConfig, defaultOptions);
+
+    const [state, dispatch] = useContext(SuitesContext);
 
     // event handlers;
     const handleOnTabPress = (e, routeName) => {
@@ -60,28 +62,28 @@ export const SuiteNavigator = ({screenDimensions, navigation, descriptors}) => {
     };
 
     return (
-        <View style={styles.container}>
-            <SideBarComponent
-                routes={routes}
-                selectedIndex={index}
-                screenDimensions={screenDimensions}
-                navigation={navigation}
-                onTabPressed={handleOnTabPress}
-                style={styles.navBar}
-            />
+        <Provider>
+            <ModalProvider stack={stack}>
+                <View style={styles.container}>
+                    <SideBarComponent
+                        routes={routes}
+                        selectedIndex={index}
+                        screenDimensions={screenDimensions}
+                        navigation={navigation}
+                        onTabPressed={handleOnTabPress}
+                        style={styles.navBar}
+                    />
 
-            <View style={styles.pageContent}>
-                <Provider>
-                    <ModalProvider stack={stack}>
+                    <View style={styles.pageContent}>
                         <ActiveScreen
                             navigation={descriptor.navigation}
                             descriptor={descriptor}
                             screenDimensions={screenDimensions}
                         />
-                    </ModalProvider>
-                </Provider>
-            </View>
-        </View>
+                    </View>
+                </View>
+            </ModalProvider>
+        </Provider>
     )
 };
 
