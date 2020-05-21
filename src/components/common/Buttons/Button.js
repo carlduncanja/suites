94f3f7f86f7e9080c96f51ab2c46317f175eb8af
Akinyele <akinyele.thompson@smsja.net>
2020-05-21 14:36:19 -0500

    -- fix login page button on press listener. -- st

--- src/components/common/Buttons/Button.js
+++ src/components/common/Buttons/Button.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react';
-import {View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import React, {Component} from 'react';
+import {View, StyleSheet, TouchableOpacity, Text} from 'react-native';
 
-const Button = ({backgroundColor, buttonPress, color, title}) =>{
+const Button = ({backgroundColor, buttonPress, color, title}) => {
     return (
-        <TouchableOpacity 
-            style={[styles.button,{backgroundColor:backgroundColor}]} 
-            onPress={buttonPress}
-        >
-            <Text style={{color:color}}>{title}</Text>
+        <TouchableOpacity onPress={buttonPress}>
+            <View style={[styles.button, {backgroundColor: backgroundColor}]}>
+                <Text style={{color: color}}>{title}</Text>
+            </View>
         </TouchableOpacity>
     )
 }
 
 export default Button
 
-const styles=StyleSheet.create({
-    button:{
+const styles = StyleSheet.create({
+    button: {
         //backgroundColor:'#F7FAFC',
         //borderRadius:4,
         //borderWidth:1,
         // borderColor:'#CBD5E0',
-        alignItems:'center',
-        justifyContent:'center',
+        alignItems: 'center',
+        justifyContent: 'center',
         //padding:10,
-        alignSelf:'center',
+        alignSelf: 'center',
         //padding:5,
         //width:91,
         //height:24,
 
     },
-    buttonText:{
+    buttonText: {
         //color: '#4A5568',
     },
 })
