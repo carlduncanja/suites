7fbd436fc5e84497937450e825c34281c8b5190c
Akinyele <akinyele.thompson@smsja.net>
2020-06-18 16:13:04 -0500

    -- remove ngroke endpoint.

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const BASE_URL = "http://88ba0fb345c0.ngrok.io/api"; //process.env.SUITES_BASE_URL;
-// const BASE_URL = "https://suite-api.azurewebsites.net/api"; //process.env.SUITES_BASE_URL;
+// const BASE_URL = "http://88ba0fb345c0.ngrok.io/api"; //process.env.SUITES_BASE_URL;
+const BASE_URL = "https://suite-api.azurewebsites.net/api"; //process.env.SUITES_BASE_URL;
 
 const baseInstance = axios.create({
     baseURL: BASE_URL,
