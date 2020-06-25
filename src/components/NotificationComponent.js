653488ddb3c456ce825eb10bccebd39b088785e6
Akinyele <akinyele.thompson@smsja.net>
2020-06-25 13:42:51 -0500

    -- create NotificationComponent.js for displaying notifications.

---
+++ src/components/NotificationComponent.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {View, StyleSheet, Text, TouchableOpacity, TouchableWithoutFeedback} from "react-native";
+import CancelIcon from "../../assets/svg/CancelIcon";
+import FolderIcon from "../../assets/svg/FolderIcon";
+import moment from "moment";
+
+
+function NotificationComponent(props) {
+
+    const {
+        icon = <FolderIcon/>,
+        timeCreated = new Date(),
+        msg = "300 items have been added to inventory."
+    } = props;
+
+    return (
+        <TouchableWithoutFeedback pointerEvents={"auto"} animation='fadeInRight' duration={400}>
+            <View style={styles.container}>
+
+                <View style={styles.headingRow}>
+                    <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+                        <View style={{paddingRight: 10, paddingTop: 4,}}>
+                            {icon}
+                        </View>
+                        <Text>Group</Text>
+                    </View>
+
+                    <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+                        <Text style={styles.text}>
+                            {/*Time*/}
+                            {moment(timeCreated).fromNow()}
+
+                        </Text>
+                        <TouchableOpacity style={{paddingLeft: 10, paddingTop: 4,}}>
+                            <CancelIcon/>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                <View style={{flex: 1, justifyContent: "center"}}>
+
+                    <Text style={styles.text}>
+                        {msg}
+                    </Text>
+
+                    {props.children}
+                </View>
+            </View>
+        </TouchableWithoutFeedback>
+    );
+}
+
+NotificationComponent.propTypes = {};
+NotificationComponent.defaultProps = {};
+
+const styles = StyleSheet.create({
+    container: {
+        width: 350,
+        minHeight: 94,
+        backgroundColor: '#FFFFFF',
+        padding: 12,
+        flexDirection: 'column',
+        borderRadius: 12,
+        shadowColor: "rgba(0,0,0,0.49)",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    headingRow: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    text: {
+        color: "#718096"
+    }
+})
+
+export default NotificationComponent;
