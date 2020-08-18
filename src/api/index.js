6ba6ac666ace2800dbe1bc3512bcd280b7504560
Akinyele <akinyele.thompson@smsja.net>
2020-08-18 13:58:27 -0500

    change base url to point to live server.

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://f8fe8641664c.ngrok.io"; //process.env.SUITES_BASE_URL;
-// const BASE_URL = "https://suite-api.azurewebsites.net"; //process.env.SUITES_BASE_URL;
+// const BASE_URL = "http://f8fe8641664c.ngrok.io"; //process.env.SUITES_BASE_URL;
+const BASE_URL = "https://suite-api.azurewebsites.net"; //process.env.SUITES_BASE_URL;
 
 const baseInstance = axios.create({
     baseURL: `${BASE_URL}/api`,
