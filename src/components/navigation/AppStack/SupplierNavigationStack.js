f985b6c3980d758797bd276f9aa6017682d6db1e
tredon <trevidlas@gmail.com>
2020-08-27 13:27:00 -0500

    "Page wasnt being tracked by git"

---
+++ src/components/navigation/AppStack/SupplierNavigationStack.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Suppliers from "../../../page/Suppliers";
+import SupplierPage from "../../Suppliers/SupplierPage";
+import NotFound from "../../../page/NotFound";
+import HeaderBackComponent from "../components/HeaderBackComponent";
+import ArchivedSuppliers from "../../../page/ArchivedSuppliers/ArchivedSuppliersPage";
+import ArchivedSupplier from "../../../page/ArchivedSuppliers/ArchivedSupplier";
+
+
+const Stack = createStackNavigator();
+
+
+export default () => {
+
+    return (
+        <Stack.Navigator
+            initialRouteName="Suppliers"
+        >
+            <Stack.Screen
+                name="Suppliers"
+                component={Suppliers}
+                options={{
+                    headerShown: false
+                }}
+            />
+
+
+            <Stack.Screen
+                name="SupplierPage"
+                component={SupplierPage}
+                options={{
+                    headerShown: false,
+                    headerLeft: (props) => (
+                        <HeaderBackComponent
+                            {...props}
+                        />
+                    ),
+                    headerStyle: {
+                        height: 100,
+
+                    },
+                    headerTitle: ""
+                }}
+            />
+            <Stack.Screen
+                name="ArchivedSuppliers"
+                component={ArchivedSuppliers}
+                options={{
+                    headerShown: false,
+                    headerLeft: (props) => (
+                        <HeaderBackComponent
+                            {...props}
+                        />
+                    ),
+                    headerStyle: {
+                        height: 100,
+
+                    },
+                    headerTitle: ""
+                }}
+            />
+            <Stack.Screen
+                name="ArchivedSupplier"
+                component={ArchivedSupplier}
+                options={{
+                    headerShown: false,
+                    headerLeft: (props) => (
+                        <HeaderBackComponent
+                            {...props}
+                        />
+                    ),
+                    headerStyle: {
+                        height: 100,
+
+                    },
+                    headerTitle: ""
+                }}
+            />
+
+        </Stack.Navigator>
+    );
+}
