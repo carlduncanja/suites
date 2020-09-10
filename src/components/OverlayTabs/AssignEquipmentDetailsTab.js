f3fbeb17c6a873f98b4549a7e226f5050c01684c
tredon <trevidlas@gmail.com>
2020-09-09 19:04:46 -0500

    Files were unstaged

---
+++ src/components/OverlayTabs/AssignEquipmentDetailsTab.js
@@ -0,0 +1,231 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import styled, { css } from '@emotion/native';
+import { withModal } from "react-native-modalfy";
+import Record from '../common/Information Record/Record';
+import ListTextRecord from '../common/Information Record/ListTextRecord';
+import Row from '../common/Row';
+import InputField2 from '../common/Input Fields/InputField2';
+import DropdownField from '../common/Input Fields/DropdownField';
+import DropdownInputField from '../common/Input Fields/DropdownInputField';
+import OptionsField from '../common/Input Fields/OptionsField';
+import { MenuOptions, MenuOption } from 'react-native-popup-menu';
+import { getTheatres } from "../../api/network";
+import { getPhysicians } from "../../api/network";
+import SearchableOptionsField from '../common/Input Fields/SearchableOptionsField';
+import { isEmpty } from 'lodash';
+import { Divider } from 'react-native-paper';
+import _ from "lodash";
+
+
+const InputWrapper = styled.View`
+height:30px;
+width:230px;
+margin:40px;
+
+`
+
+const ViewBreaker = styled.View`
+flex-direction:row;
+align-items:center;
+padding:10px;
+justify-content:space-between;
+margin-top:-50px;
+`
+const DoneButtonWrapper = styled.TouchableOpacity`
+width:616px;
+border-radius:8px;
+height:48px;
+margin-top:20px;
+align-self:center;
+align-items:center;
+justify-self:flex-end;
+justify-content:center;
+background-color:${({ theme }) => theme.colors["--color-blue-500"]}
+`;
+
+const DoneButtonText = styled.Text`
+font:${({ theme }) => theme.font["--text-base-bold"]};
+color:${({ theme }) => theme.colors["--default-shade-white"]};
+`
+
+
+function AddEquipmentDetailsTab({ data, onFieldChange, onLocationUpdate, locations, physcians, onPhysicianUpdate, equipmentDetails, onDonePress }) {
+
+    const [searchValue, setSearchValue] = useState('');
+    const [searchResult, setSearchResults] = useState([]);
+    const [searchQuery, setSearchQuery] = useState();
+
+    useEffect(() => {
+
+        if (!isEmpty(searchValue)) {
+            const searchFunction = data['Assignment'] === "Location" ? fetchTheatres : fetchPhysicians;
+
+            const search = _.debounce(searchFunction, 300);
+
+            setSearchQuery(prevSearch => {
+                if (prevSearch && prevSearch.cancel) {
+                    prevSearch.cancel();
+                }
+                return search;
+            });
+            search();
+        } else setSearchResults([])
+
+
+
+
+    }, [searchValue])
+
+
+    const fetchTheatres = () => {
+        getTheatres(searchValue, 5)
+            .then((theatreResult = []) => {
+                const { data = [], pages = 0 } = theatreResult
+                const results = data.map(item => ({
+                    name: item.name,
+                    ...item
+                }));
+                setSearchResults(results || []);
+
+            })
+            .catch(error => {
+                // TODO handle error
+                console.log("failed to get Theatres");
+                setSearchResults([]);
+            })
+    }
+
+    const fetchPhysicians = () => {
+        getPhysicians(searchValue, 5)
+            .then((physicianResult = []) => {
+                const { data = [], pages = 0 } = physicianResult
+                const results = data.map(item => ({
+                    name: `Dr. ${item.surname}`,
+                    ...item
+                }));
+                setSearchResults(results || []);
+
+            })
+            .catch(error => {
+                // TODO handle error
+
+                console.log("failed to get physicians", error);
+                setSearchResults([]);
+            })
+    }
+
+
+    const handleQuantityValidation = (value) => {
+        !isNaN(value) ? onFieldChange('Quantity')(value) : ""
+    }
+    return (
+        <>
+            <View style={{ flexDirection: "column", height: "88%", width: "100%", alignItems: "center", justifyContent: "flex-start" }}>
+                <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
+                    <InputWrapper>
+                        <InputField2
+                            value=""
+                            labelWidth={30}
+                            placeholder={equipmentDetails?.name}
+                            label="Equipment"
+                            backgroundColor="--color-gray-200"
+                        />
+                    </InputWrapper>
+
+                    <InputWrapper>
+                        <InputField2
+                            value={""}
+                            labelWidth={30}
+                            placeholder={isEmpty(equipmentDetails?.categories[0]) ? "--" : equipmentDetails.categories[0]}
+                            label="Category"
+                            backgroundColor="--color-gray-200"
+                        />
+                    </InputWrapper>
+                </View>
+                <ViewBreaker>
+                    <InputWrapper>
+                        <OptionsField
+                            key={data['Assignment']}
+                            labelWidth={50}
+                            label="Assignment"
+                            text={data['Assignment']}
+                            oneOptionsSelected={onFieldChange('Assignment')}
+                            menuOption={<MenuOptions>
+                                <MenuOption value={"Location"} text='Location' />
+                                <MenuOption value={"Person"} text='Person' />
+                            </MenuOptions>}
+
+                        />
+                    </InputWrapper>
+
+                    <InputWrapper>
+                        <InputField2
+                            key={data['Quantity']}
+                            value={data['Quantity']}
+                            labelWidth={30}
+                            onChangeText={(value) => { handleQuantityValidation(value) }}
+                            onFieldChange={onFieldChange('Quantity')}
+                            onClear={() => onFieldChange('Quantity')('')}
+                            label="Quantity"
+                            keyboardType="number-pad"
+                        />
+                    </InputWrapper>
+                </ViewBreaker>
+                <ViewBreaker>
+                    <InputWrapper>
+                        <SearchableOptionsField
+                            label="Assigned"
+                            labelWidth={30}
+                            value={data['Assignment'] === "Location" ? locations : physcians}
+                            text={searchValue}
+                            oneOptionsSelected={(value) => {
+                                const location = {
+                                    _id: value._id,
+                                    name: value.name,
+
+                                }
+                                const physician = {
+                                    _id: value._id,
+                                    name: value.name
+                                }
+
+
+                                data['Assignment'] === "Location" ? onLocationUpdate(location) : onPhysicianUpdate(physician)
+
+                            }}
+                            onChangeText={(value) => setSearchValue(value)}
+                            onClear={() => setSearchValue('')}
+                            options={searchResult}
+                        />
+                    </InputWrapper>
+                    <InputWrapper>
+                        <OptionsField
+                            key={data['Status']}
+                            label="Status"
+                            text={data['Status']}
+                            oneOptionsSelected={onFieldChange('Status')}
+                            menuOption={<MenuOptions>
+                                <MenuOption value={"Available"} text='Available' />
+                                <MenuOption value={"Servicing"} text='Servicing' />
+                                <MenuOption value={"Damaged"} text='Damaged' />
+                                <MenuOption value={"In Use"} text='In Use' />
+                            </MenuOptions>}
+
+                        />
+                    </InputWrapper>
+
+
+                </ViewBreaker>
+
+
+            </View >
+            <Divider />
+            <DoneButtonWrapper onPress={onDonePress}>
+                <DoneButtonText>DONE</DoneButtonText>
+            </DoneButtonWrapper>
+        </>
+    )
+}
+
+export default withModal(AddEquipmentDetailsTab)
---
+++ src/page/AssignEquipmentPage.js
@@ -0,0 +1,255 @@
+import React, { useContext, useEffect, useState } from 'react';
+import styled, { css } from '@emotion/native';
+import { withModal } from "react-native-modalfy";
+import AddEquipmentDetailsTab from "../components/OverlayTabs/AddEquipmentDetailsTab";
+import { useTheme } from "emotion-theming";
+import { Text, View, TouchableOpacity } from "react-native";
+import ConfirmationComponent from "../components/ConfirmationComponent";
+import { createEquipment } from "../api/network";
+import TabsContainer from "../components/common/Tabs/TabsContainerComponent"
+import Footer from '../components/common/Page/Footer';
+import { Divider, Modal } from 'react-native-paper';
+import AssignEquipmentDetailsTab from '../components/OverlayTabs/AssignEquipmentDetailsTab';
+
+
+const AssignEquipmentPageWrapper = styled.View`
+        margin:0;
+        background-color: ${({ theme }) => theme.colors['--default-shade-white']};
+    `;
+
+const AssignEquipmentPageContainer = styled.View`
+        height:100%;
+        width:100%;
+    `;
+
+const HeaderContainer = styled.View`
+    flex-direction:row;
+    height:47px;
+    width:681px;
+    align-items:center;
+    padding:10px;
+`;
+
+const HeaderText = styled.Text`
+    font:${({ theme }) => theme.font["--text-xl-medium"]};
+    color:${({ theme }) => theme.colors["--company"]}
+    `;
+
+const CloseButtonWrapper = styled.View`
+    align-items:flex-end;
+    width:540px;
+    `;
+const CloseButtonContainer = styled.TouchableOpacity`
+    background-color:${({ theme }) => theme.colors["--color-gray-300"]};
+    width:68px;
+    height:26px;
+    padding:4px 14px;
+    border-radius:6px;
+    justify-content:center;
+    `;
+const CloseText = styled.Text`
+    color:${({ theme }) => theme.colors["--color-gray-600"]};
+    font:${({ theme }) => theme.font["--text-sm-bold"]}
+    `;
+
+const AssignEquipmentPageContentWrapper = styled.View`
+        flex:1;
+        margin : 0;
+        padding-top: ${({ theme }) => theme.space['--space-32']};
+        padding-left: ${({ theme }) => theme.space['--space-24']};
+        padding-right: ${({ theme }) => theme.space['--space-24']};
+`
+
+const AssignEquipmentPageContentContainer = styled.View`
+        display: flex;
+        flex:1;
+`
+
+const TabsViewContainer = styled.View`
+    height: 54px;
+`;
+
+const testData = {
+    Sku: "",
+    Assignment: "Location",
+    Quantity: "1",
+    Status: "Available"
+}
+
+const AssignEquipmentPage = ({ navigation, route, modal }) => {
+    const { equipment, onCreated } = route.params;
+    const currentTabs = ["Details"];
+    const theme = useTheme();
+    const [equipmentData, setEquipmentData] = useState(testData);
+    const [locations, setLocations] = useState([]);
+    const [physicians, setPhysicians] = useState([]);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [currentTab, setCurrentTab] = useState(currentTabs[0]);
+    const [isEditMode, setEditMode] = useState(false);
+
+    const onFieldChange = (fieldName) => (value) => {
+        const updateFields = { ...equipmentData }
+        setEquipmentData({
+            ...updateFields,
+            [fieldName]: value
+        })
+        console.log(equipmentData);
+    }
+
+    const onLocationUpdate = (value) => {
+        const updatedLocations = [...locations]
+
+        // check if value is at index
+        updatedLocations[selectedIndex] = value;
+        console.log("Updated locations:", updatedLocations)
+        setLocations(updatedLocations);
+    }
+
+    const onPhysicianUpdate = (value) => {
+        const updatePhysicians = [...physicians]
+        console.log("Updated Physician:", updatePhysicians);
+        updatePhysicians[selectedIndex] = value;
+        setPhysicians(updatePhysicians)
+    }
+
+
+
+
+
+    useEffect(() => {
+
+    }, [])
+
+    const onTabPress = (selectedTab) => {
+        if (!isEditMode) setCurrentTab(selectedTab);
+    };
+
+    const closeTapped = () => {
+        navigation.navigate("Equipment")
+    }
+
+    const onDonePress = () => {
+        const fieldsToPass =
+        {
+            sku: equipmentData['Sku'],
+            quantity: equipmentData['Quantity'],
+            type: equipment._id,
+            status: equipmentData['Status']
+        };
+        createEquipmentCall(fieldsToPass);
+
+    }
+
+    const createdSuccessfully = () => {
+        modal.closeModals("ConfirmationModal");
+        navigation.navigate("Equipment");
+        onCreated();
+
+    }
+
+    const onCancel = () => {
+        modal.closeModals("ConfirmationModal");
+    }
+
+    const createEquipmentCall = (updatedFields) => {
+        createEquipment(updatedFields)
+            .then(data => {
+                modal.openModal("ConfirmationModal", {
+                    content: (
+                        <ConfirmationComponent
+                            isError={false}//boolean to show whether an error icon or success icon
+                            isEditUpdate={false}//use this specification to either get the confirm an edit or update
+                            onCancel={onCancel}
+                            onAction={createdSuccessfully}
+                            message="Please choose a supplier "//general message you can send to be displayed
+                            action="Archive"
+                        />
+                    ), onClose: () => {
+                        console.log("Modal closed");
+                    },
+                })
+
+
+            })
+            .catch(error => {
+                console.log(error);
+
+                // TODO handle error
+                modal.openModal("ConfirmationModal", {
+                    content: (
+                        <ConfirmationComponent
+                            isError={true}//boolean to show whether an error icon or success icon
+                            isEditUpdate={false}//use this specification to either get the confirm an edit or update
+                            onCancel={onCancel}
+                            onAction={createdSuccessfully}
+                            message="There was an error performing this task "//general message you can send to be displayed
+                            action="Archive"
+                        />
+                    ), onClose: () => {
+                        console.log("Modal closed");
+                    },
+                })
+            })
+            .finally(_ => {
+            })
+    };
+
+
+    const getTabContent = (selectedTab) => {
+        switch (selectedTab) {
+            case "Details":
+                return <AssignEquipmentDetailsTab
+                    onDonePress={onDonePress}
+                    equipmentDetails={equipment}
+                    data={equipmentData}
+                    locations={locations[selectedIndex]}
+                    onFieldChange={onFieldChange}
+                    onLocationUpdate={onLocationUpdate}
+                    onPhysicianUpdate={onPhysicianUpdate}
+                    physicians={physicians[selectedIndex]}
+                />
+            default:
+                return <View />
+        }
+    };
+    return (
+        <AssignEquipmentPageWrapper theme={theme}>
+            <AssignEquipmentPageContainer theme={theme}>
+
+
+                <HeaderContainer >
+                    <HeaderText theme={theme} >Assign Equipment</HeaderText>
+                    <CloseButtonWrapper>
+                        <CloseButtonContainer onPress={closeTapped} >
+                            <CloseText>Close</CloseText>
+                        </CloseButtonContainer>
+                    </CloseButtonWrapper>
+                </HeaderContainer>
+
+                <TabsViewContainer>
+                    <TabsContainer
+                        tabs={currentTabs}
+                        selectedTab={currentTab}
+                        onPressChange={onTabPress}
+                    />
+                </TabsViewContainer>
+
+                <AssignEquipmentPageContentWrapper>
+                    <AssignEquipmentPageContentContainer>
+                        {getTabContent(currentTab)}
+
+
+                    </AssignEquipmentPageContentContainer>
+                </AssignEquipmentPageContentWrapper>
+
+            </AssignEquipmentPageContainer>
+        </AssignEquipmentPageWrapper>
+
+
+
+
+    );
+}
+
+
+export default withModal(AssignEquipmentPage);
