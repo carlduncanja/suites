5ce838be20faf1ea452b106f6ee700646b61562d
Howard Edwards <howard.edwards@smsja.net>
2020-09-16 21:16:36 -0500

    fix for live update of physician details for issue https://gitlab.com/smsja-ss/clients/the-suites/the-suites/-/issues/235

--- src/page/Physicians.js
+++ src/page/Physicians.js
@@ -1,93 +1,91 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { View, Text, StyleSheet } from "react-native";
+import React, {useState, useContext, useEffect} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
+import {connect} from 'react-redux';
+import _ from 'lodash';
+import {withModal} from 'react-native-modalfy';
 import Page from '../components/common/Page/Page';
 import ListItem from '../components/common/List/ListItem';
 import RoundedPaginator from '../components/common/Paginators/RoundedPaginator';
 import FloatingActionButton from '../components/common/FloatingAction/FloatingActionButton';
 import PhysicianActionIcon from '../../assets/svg/physicianListAction';
-import LongPressWithFeedback from "../components/common/LongPressWithFeedback";
-import ActionContainer from "../components/common/FloatingAction/ActionContainer";
-import ActionItem from "../components/common/ActionItem";
-import CreateWorkItemDialog from "../components/Physicians/CreateWorkItemDialog";
+import LongPressWithFeedback from '../components/common/LongPressWithFeedback';
+import ActionContainer from '../components/common/FloatingAction/ActionContainer';
+import ActionItem from '../components/common/ActionItem';
+import CreateWorkItemDialog from '../components/Physicians/CreateWorkItemDialog';
 import NavPage from '../components/common/Page/NavPage';
 
-import WasteIcon from "../../assets/svg/wasteIcon";
-import AddIcon from "../../assets/svg/addIcon";
-import AssignIcon from "../../assets/svg/assignIcon";
+import WasteIcon from '../../assets/svg/wasteIcon';
+import AddIcon from '../../assets/svg/addIcon';
+import AssignIcon from '../../assets/svg/assignIcon';
 
-import { useNextPaginator, usePreviousPaginator, checkboxItemPress, selectAll } from '../helpers/caseFilesHelpers';
+import {useNextPaginator, usePreviousPaginator, checkboxItemPress, selectAll} from '../helpers/caseFilesHelpers';
 
-import { connect } from 'react-redux';
-import { setPhysicians } from "../redux/actions/physiciansActions";
-import { getPhysicians } from "../api/network";
-import _ from "lodash";
+import {setPhysicians} from '../redux/actions/physiciansActions';
+import {getPhysicians} from '../api/network';
 
-import { withModal } from 'react-native-modalfy';
 import CreatePhysicianDialogContainer from '../components/Physicians/CreatePhyscianDialogContainer';
 
-const Physicians = (props) => {
-
+const Physicians = props => {
     // ############# Const data
 
     const recordsPerPage = 10;
     const listHeaders = [
         {
-            name: "Name",
-            alignment: "flex-start"
+            name: 'Name',
+            alignment: 'flex-start'
         },
         {
-            name: "Type",
-            alignment: "center"
+            name: 'Type',
+            alignment: 'center'
         },
         {
-            name: "Status",
-            alignment: "center"
+            name: 'Status',
+            alignment: 'center'
         },
         {
-            name: "Actions",
-            alignment: "center"
+            name: 'Actions',
+            alignment: 'center'
         }
     ];
-    const floatingActions = []
+    const floatingActions = [];
 
     //  ############ Props
 
-    const { physicians, setPhysicians, navigation, modal } = props;
+    const {physicians, setPhysicians, navigation, modal} = props;
 
     //  ############ State
 
     const [isFetchingData, setFetchingData] = useState(false);
-    const [isFloatingActionDisabled, setFloatingAction] = useState(false)
+    const [isFloatingActionDisabled, setFloatingAction] = useState(false);
 
     const [totalPages, setTotalPages] = useState(1);
-    const [currentPageListMin, setCurrentPageListMin] = useState(0)
-    const [currentPageListMax, setCurrentPageListMax] = useState(recordsPerPage)
-    const [currentPagePosition, setCurrentPagePosition] = useState(1)
+    const [currentPageListMin, setCurrentPageListMin] = useState(0);
+    const [currentPageListMax, setCurrentPageListMax] = useState(recordsPerPage);
+    const [currentPagePosition, setCurrentPagePosition] = useState(1);
     const [isNextDisabled, setNextDisabled] = useState(false);
     const [isPreviousDisabled, setPreviousDisabled] = useState(true);
 
-    const [searchValue, setSearchValue] = useState("");
+    const [searchValue, setSearchValue] = useState('');
     const [searchResults, setSearchResult] = useState([]);
     const [searchQuery, setSearchQuery] = useState({});
 
-    const [selectedPhysiciansId, setSelectedPhysiciansId] = useState([])
+    const [selectedPhysiciansId, setSelectedPhysiciansId] = useState([]);
 
     // ############# Lifecycle methods
 
     useEffect(() => {
         if (!physicians.length) {
-            fetchPhysiciansData(currentPagePosition)
+            fetchPhysiciansData(currentPagePosition);
         }
-        setTotalPages(Math.ceil(physicians.length / recordsPerPage))
+        setTotalPages(Math.ceil(physicians.length / recordsPerPage));
     }, []);
 
     useEffect(() => {
-
         if (!searchValue) {
             // empty search values and cancel any out going request.
             setSearchResult([]);
-            fetchPhysiciansData(1)
+            fetchPhysiciansData(1);
             if (searchQuery.cancel) searchQuery.cancel();
             return;
         }
@@ -103,82 +101,85 @@ const Physicians = (props) => {
             return search;
         });
 
-        search()
-        setCurrentPagePosition(1)
+        search();
+        setCurrentPagePosition(1);
     }, [searchValue]);
 
     // ############# Event Handlers
 
-    const onSearchInputChange = (input) => {
-        setSearchValue(input)
-    }
+    const onSearchInputChange = input => {
+        setSearchValue(input);
+    };
 
     const handleDataRefresh = () => {
-        fetchPhysiciansData()
+        fetchPhysiciansData();
     };
 
     const handleOnSelectAll = () => {
-        let updatedPhysiciansList = selectAll(physicians, selectedPhysiciansId)
-        setSelectedPhysiciansId(updatedPhysiciansList)
-    }
+        const updatedPhysiciansList = selectAll(physicians, selectedPhysiciansId);
+        setSelectedPhysiciansId(updatedPhysiciansList);
+    };
 
-    const handleOnCheckBoxPress = (item) => () => {
-        const { _id } = item;
-        let updatedPhysiciansList = checkboxItemPress(item, _id, selectedPhysiciansId)
-        setSelectedPhysiciansId(updatedPhysiciansList)
-    }
+    const handleOnCheckBoxPress = item => () => {
+        const {_id} = item;
+        const updatedPhysiciansList = checkboxItemPress(item, _id, selectedPhysiciansId);
+        setSelectedPhysiciansId(updatedPhysiciansList);
+    };
 
     const handleOnItemPress = (item, isOpenEditable) => {
         // modal.openModal('BottomSheetModal',{
         //     content : <PhysicianBottomSheet physician = {item} isOpenEditable = {isOpenEditable}/>
         // })
-        props.navigation.navigate('PhysicianPage', { initial: false, physician: item, isEdit: isOpenEditable });
-    }
+        props.navigation.navigate('PhysicianPage', {
+            initial: false,
+            physician: item,
+            isEdit: isOpenEditable,
+            reloadPhysicians: () => fetchPhysiciansData(currentPagePosition)
+        });
+    };
 
     const goToNextPage = () => {
         if (currentPagePosition < totalPages) {
-            let { currentPage, currentListMin, currentListMax } = useNextPaginator(currentPagePosition, recordsPerPage, currentPageListMin, currentPageListMax)
+            const {currentPage, currentListMin, currentListMax} = useNextPaginator(currentPagePosition, recordsPerPage, currentPageListMin, currentPageListMax);
             setCurrentPagePosition(currentPage);
             setCurrentPageListMin(currentListMin);
             setCurrentPageListMax(currentListMax);
-            fetchPhysiciansData(currentPage)
+            fetchPhysiciansData(currentPage);
         }
     };
 
     const goToPreviousPage = () => {
         if (currentPagePosition === 1) return;
 
-        let { currentPage, currentListMin, currentListMax } = usePreviousPaginator(currentPagePosition, recordsPerPage, currentPageListMin, currentPageListMax)
+        const {currentPage, currentListMin, currentListMax} = usePreviousPaginator(currentPagePosition, recordsPerPage, currentPageListMin, currentPageListMax);
         setCurrentPagePosition(currentPage);
         setCurrentPageListMin(currentListMin);
         setCurrentPageListMax(currentListMax);
-        fetchPhysiciansData(currentPage)
-
+        fetchPhysiciansData(currentPage);
     };
 
     const toggleActionButton = () => {
-        setFloatingAction(true)
-        modal.openModal("ActionContainerModal",
+        setFloatingAction(true);
+        modal.openModal('ActionContainerModal',
             {
                 actions: getFabActions(),
-                title: "PHYSICIAN ACTIONS",
+                title: 'PHYSICIAN ACTIONS',
                 onClose: () => {
-                    setFloatingAction(false)
+                    setFloatingAction(false);
                 }
-            })
-    }
+            });
+    };
 
     // ############# Helper functions
 
-    const fetchPhysiciansData = (pagePosition) => {
-
-        let currentPosition = pagePosition ? pagePosition : 1;
+    const fetchPhysiciansData = pagePosition => {
+        const currentPosition = pagePosition || 1;
         setCurrentPagePosition(currentPosition);
 
         setFetchingData(true);
         getPhysicians(searchValue, recordsPerPage, currentPosition)
             .then(physicianResult => {
-                const { data = [], pages = 0 } = physicianResult
+                const {data = [], pages = 0} = physicianResult;
 
                 if (pages === 1) {
                     setPreviousDisabled(true);
@@ -191,73 +192,103 @@ const Physicians = (props) => {
                     setPreviousDisabled(false);
                 } else if (currentPosition < pages) {
                     setNextDisabled(false);
-                    setPreviousDisabled(false)
+                    setPreviousDisabled(false);
                 } else {
                     setNextDisabled(true);
                     setPreviousDisabled(true);
                 }
 
                 setPhysicians(data);
-                data.length === 0 ? setTotalPages(1) : setTotalPages(pages)
+                data.length === 0 ? setTotalPages(1) : setTotalPages(pages);
             })
             .catch(error => {
-                console.log("failed to get physicians", error);
-                setTotalPages(1)
-                setPreviousDisabled(true)
-                setNextDisabled(true)
+                console.log('failed to get physicians', error);
+                setTotalPages(1);
+                setPreviousDisabled(true);
+                setNextDisabled(true);
             })
             .finally(_ => {
-                setFetchingData(false)
-            })
+                setFetchingData(false);
+            });
     };
 
-    const renderPhysiciansFn = (item) => {
-        return <ListItem
+    const renderPhysiciansFn = item => (
+        <ListItem
             hasCheckBox={true}
             isChecked={selectedPhysiciansId.includes(item._id)}
             onCheckBoxPress={handleOnCheckBoxPress(item)}
             onItemPress={() => handleOnItemPress(item, false)}
             itemView={physiciansItem(item)}
         />
-    }
+    );
 
-    const statusColor = (status) => {
-        return status === 'Active' ? '#4E5664' : '#E53E3E'
-    }
+    const statusColor = status => (status === 'Active' ? '#4E5664' : '#E53E3E');
 
-    const physiciansItem = (item) => {
-
-        const { _id = "", surname = "", type = "Neurosurgeon", status = "Active" } = item
+    const physiciansItem = item => {
+        const {_id = '', surname = '', type = 'Neurosurgeon', status = 'Active'} = item;
         return (
             <>
                 <View style={[styles.item, {}]}>
                     {/*<Text numberOfLines={1} style={[styles.itemText, { fontSize: 12, color: "#718096" }]}>#{_id}</Text>*/}
-                    <Text numberOfLines={1} style={[styles.itemText, { fontSize: 16, color: "#3182CE" }]}>Dr. {surname}</Text>
+                    <Text
+                        numberOfLines={1}
+                        style={[styles.itemText, {fontSize: 16, color: '#3182CE'}]}
+                    >Dr. {surname}</Text>
                 </View>
-                <View style={[styles.item, { alignItems: 'center' }]}>
-                    <Text numberOfLines={1} style={[styles.itemText, { fontSize: 16, color: '#4E5664' }]}>{type}</Text>
+                <View style={[styles.item, {alignItems: 'center'}]}>
+                    <Text numberOfLines={1} style={[styles.itemText, {fontSize: 16, color: '#4E5664'}]}>{type}</Text>
                 </View>
-                <View style={[styles.item, { alignItems: 'center' }]}>
-                    <Text numberOfLines={1} style={[styles.itemText, { fontSize: 14, color: statusColor(status) }]}>{status}</Text>
+                <View style={[styles.item, {alignItems: 'center'}]}>
+                    <Text
+                        numberOfLines={1}
+                        style={[styles.itemText, {fontSize: 14, color: statusColor(status)}]}
+                    >{status}</Text>
                 </View>
-                <View style={[styles.item, { alignItems: 'center' }]}>
-                    <PhysicianActionIcon />
+                <View style={[styles.item, {alignItems: 'center'}]}>
+                    <PhysicianActionIcon/>
                 </View>
             </>
-        )
-
-    }
+        );
+    };
 
     const getFabActions = () => {
-
-        const deleteAction =
-            <LongPressWithFeedback pressTimer={700} onLongPress={() => { }}>
-                <ActionItem title={"Hold to Delete"} icon={<WasteIcon />} onPress={() => { }} touchable={false} />
-            </LongPressWithFeedback>;
-        const assignActionCase = <ActionItem title={"Assign Case"} icon={<AssignIcon />} onPress={() => { }} />;
-        const createActionWorkItem = <ActionItem title={"Add Work Item"} icon={<AddIcon />} onPress={openCreateNewWorkItem} />;
-        const createActionPhysician = <ActionItem title={"Add Physician"} icon={<AddIcon />} onPress={openCreatePhysicians} />;
-
+        const deleteAction = (
+            <LongPressWithFeedback
+                pressTimer={700}
+                onLongPress={() => {
+                }}
+            >
+                <ActionItem
+                    title="Hold to Delete"
+                    icon={<WasteIcon/>}
+                    onPress={() => {
+                    }}
+                    touchable={false}
+                />
+            </LongPressWithFeedback>
+        );
+        const assignActionCase = (
+            <ActionItem
+                title="Assign Case"
+                icon={<AssignIcon/>}
+                onPress={() => {
+                }}
+            />
+        );
+        const createActionWorkItem = (
+            <ActionItem
+                title="Add Work Item"
+                icon={<AddIcon/>}
+                onPress={openCreateNewWorkItem}
+            />
+        );
+        const createActionPhysician = (
+            <ActionItem
+                title="Add Physician"
+                icon={<AddIcon/>}
+                onPress={openCreatePhysicians}
+            />
+        );
 
         return <ActionContainer
             floatingActions={[
@@ -266,8 +297,8 @@ const Physicians = (props) => {
                 createActionPhysician,
                 createActionWorkItem
             ]}
-            title={"PHYSICIAN ACTIONS"}
-        />
+            title="PHYSICIAN ACTIONS"
+        />;
     };
 
     const openCreateNewWorkItem = () => {
@@ -275,50 +306,49 @@ const Physicians = (props) => {
 
         // For some reason there has to be a delay between closing a modal and opening another.
         setTimeout(() => {
-
             modal
                 .openModal(
                     'OverlayModal',
                     {
-                        content: <CreateWorkItemDialog onCancel={() => setFloatingAction(false)}
-                        />,
+                        content: <CreateWorkItemDialog onCancel={() => setFloatingAction(false)}/>,
                         onClose: () => setFloatingAction(false)
-                    })
-        }, 200)
-    }
+                    }
+                );
+        }, 200);
+    };
 
     const openCreatePhysicians = () => {
         modal.closeModals('ActionContainerModal');
 
         // For some reason there has to be a delay between closing a modal and opening another.
         setTimeout(() => {
-
             modal
                 .openModal(
                     'OverlayModal',
                     {
                         content: <CreatePhysicianDialogContainer
                             onCancel={() => setFloatingAction(false)}
-                            onCreated={(item) => handleOnItemPress(item, true)}
+                            onCreated={item => handleOnItemPress(item, true)}
                         />,
                         onClose: () => setFloatingAction(false)
-                    })
-        }, 200)
-    }
+                    }
+                );
+        }, 200);
+    };
 
     // ############# Prepare list data
 
-    let physiciansToDisplay = [...physicians];
+    const physiciansToDisplay = [...physicians];
     // physiciansToDisplay = physiciansToDisplay.slice(currentPageListMin, currentPageListMax);
 
     return (
         <NavPage
             isFetchingData={isFetchingData}
             onRefresh={handleDataRefresh}
-            placeholderText={"Search by Physician"}
+            placeholderText="Search by Physician"
             changeText={onSearchInputChange}
             inputText={searchValue}
-            routeName={"Physicians"}
+            routeName="Physicians"
             listData={physiciansToDisplay}
             listHeaders={listHeaders}
             itemsSelected={selectedPhysiciansId}
@@ -336,38 +366,27 @@ const Physicians = (props) => {
             isNextDisabled={isNextDisabled}
             isPreviousDisabled={isPreviousDisabled}
         />
-    )
+    );
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
+    const physicians = state.physicians.map(item => ({
+        ...item,
+        // type : 'Neurosurgeon',
+        // status : 'Active'
+    }));
 
-    const physicians = state.physicians.map(item => {
-        return {
-            ...item,
-            // type : 'Neurosurgeon',
-            // status : 'Active'
-        }
-    })
-
-    return {
-        physicians
-    }
+    return {physicians};
     // physicians: state.physicians
 };
 
-const mapDispatcherToProp = {
-    setPhysicians
-};
+const mapDispatcherToProp = {setPhysicians};
 
-export default connect(mapStateToProps, mapDispatcherToProp)(withModal(Physicians))
+export default connect(mapStateToProps, mapDispatcherToProp)(withModal(Physicians));
 
 const styles = StyleSheet.create({
-    item: {
-        flex: 1
-    },
-    itemText: {
-
-    },
+    item: {flex: 1},
+    itemText: {},
     footer: {
         flex: 1,
         alignSelf: 'flex-end',
@@ -378,4 +397,4 @@ const styles = StyleSheet.create({
         right: 0,
         marginRight: 30,
     },
-})
+});
