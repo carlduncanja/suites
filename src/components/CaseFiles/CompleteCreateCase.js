630e4d936b4ecb7554606178f5ad42ae7372c05d
Howard Edwards <howard.edwards@smsja.net>
2020-09-28 11:48:34 -0500

    fix double second double quotation mark around patient's name when creating case

--- src/components/CaseFiles/CompleteCreateCase.js
+++ src/components/CaseFiles/CompleteCreateCase.js
@@ -1,59 +1,57 @@
-import React,{  } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import CaseFileIcon from '../../../assets/svg/caseFolder'
+import React, {} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import CaseFileIcon from '../../../assets/svg/caseFolder';
 
-const CompleteCreateCase = ({name}) =>{
-    return (
-        <View style={styles.container}>
+const CompleteCreateCase = ({name}) => (
+    <View style={styles.container}>
 
-            <View style={styles.icon}>
-                <CaseFileIcon/>
-            </View>
-
-            <View style={styles.textBox}>
-                <Text style={styles.text}>
-                    Tap <Text style={{color:"#3182CE"}}>CONTINUE</Text> to finish filling out your new Case File for "{name}""
-                </Text>
-            </View>
-
+        <View style={styles.icon}>
+            <CaseFileIcon/>
         </View>
-    )
-}
 
-export default CompleteCreateCase
+        <View style={styles.textBox}>
+            <Text style={styles.text}>
+                Tap <Text style={{color: '#3182CE'}}>CONTINUE</Text> to finish filling out your new Case File for "{name}"
+            </Text>
+        </View>
+
+    </View>
+);
+
+export default CompleteCreateCase;
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         height: 260,
         backgroundColor: '#FFFFFF',
         flexDirection: 'column',
         padding: 24,
-        alignItems:'center',
-        justifyContent:"space-around"
+        alignItems: 'center',
+        justifyContent: 'space-around'
     },
-    icon : {
-        backgroundColor:'#EEF2F6',
-        height:124,
-        borderRadius:62,
-        padding:30,
-        marginBottom:20
+    icon: {
+        backgroundColor: '#EEF2F6',
+        height: 124,
+        borderRadius: 62,
+        padding: 30,
+        marginBottom: 20
     },
-    textBox:{
-        borderWidth:1,
-        borderColor:"#CCD6E0",
-        borderRadius:8,
-        height:64, 
-        width:304,
-        padding:12,
-        paddingLeft:20,
-        paddingRight:20,
-        alignItems:'center',
-        justifyContent:'center'
+    textBox: {
+        borderWidth: 1,
+        borderColor: '#CCD6E0',
+        borderRadius: 8,
+        height: 64,
+        width: 304,
+        padding: 12,
+        paddingLeft: 20,
+        paddingRight: 20,
+        alignItems: 'center',
+        justifyContent: 'center'
     },
-    text:{
-        color:"#4E5664",
-        fontSize:14,
-        fontWeight:'500',
-        textAlign:'center'
+    text: {
+        color: '#4E5664',
+        fontSize: 14,
+        fontWeight: '500',
+        textAlign: 'center'
     }
-})
+});
