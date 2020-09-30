a53c486bccb4eb6db030cf34046bee1fba6b2e13
Akinyele <akinyele.thompson@smsja.net>
2020-09-30 14:49:21 -0500

    fixe for content scrolling on inventory when item expanded.

--- src/components/common/List/CollapsibleListItemChildView.js
+++ src/components/common/List/CollapsibleListItemChildView.js
@@ -2,35 +2,43 @@ import React, {useState} from 'react';
 import {View, StyleSheet, TouchableOpacity, Text, ScrollView} from 'react-native';
 import LineDivider from '../LineDivider';
 import Collapsible from 'react-native-collapsible';
-import styled, { css } from '@emotion/native';
+import styled, {css} from '@emotion/native';
 import CheckBoxComponent from "../Checkbox";
-import { useTheme } from 'emotion-theming';
+import {useTheme} from 'emotion-theming';
 
 const ChildContentWrapper = styled.View`
     margin : 0;
 `;
 const ChildContentContainer = styled.ScrollView`
-    max-height : ${ ({children}) => children.props.data.length > 0 ? `400px` : '20px'};
+    //max-height : ${({children}) => children?.props?.data?.length > 0 ? `400px` : '200px'};
+    height: 200px;
     border-bottom-width : 1px;
-    border-bottom-color : ${ ({theme}) => theme.colors['--color-gray-300']};
+    border-bottom-color : ${({theme}) => theme.colors['--color-gray-300']};
     border-bottom-right-radius : 8px;
     border-bottom-left-radius : 8px;
 `;
 
-function CollapsibleListItemChildView ({isCollapsed=false, children =()=>{}, onAnimationEnd}){
+function CollapsibleListItemChildView({
+                                          isCollapsed = false,
+                                          children = () => {
+                                          }, onAnimationEnd
+                                      }) {
     const theme = useTheme();
 
     return (
 
         <Collapsible onAnimationEnd={onAnimationEnd} collapsed={isCollapsed}>
+
             <LineDivider/>
+
             {
                 !isCollapsed &&
-                <ChildContentContainer children = {children}>
-                    {children}
+                <ChildContentContainer children={children}>
+                    {
+                        children
+                    }
                 </ChildContentContainer>
             }
-
         </Collapsible>
     )
 }
