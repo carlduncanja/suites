5647946259030947870733765b478ee623a13bca
Danielle Scott <danielle.scott@smsja.net>
2020-11-20 15:42:00 -0500

    --updated the base url.

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = "http://26ef92ce9036.ngrok.io"; //process.env.SUITES_BASE_URL;
-// const BASE_URL = "https://suites-api.azurewebsites.net"; //process.env.SUITES_BASE_URL;
+// const BASE_URL = "http://26ef92ce9036.ngrok.io"; //process.env.SUITES_BASE_URL;
+const BASE_URL = "https://suites-api.azurewebsites.net"; //process.env.SUITES_BASE_URL;
 const DOCUMENT_GENERATION_BASE_URL = 'https://sms-document-generation-service.azurewebsites.net';
 
 const baseInstance = axios.create({
