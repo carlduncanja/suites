02dece88d49769d570b4583c5ab916d5f693331d
Dev <dev@Devs-MacBook-Pro.local>
2020-12-30 14:59:42 -0500

    --updated storage equipment tab to use live data.

--- src/components/OverlayTabs/StorageEquipmentTab.js
+++ src/components/OverlayTabs/StorageEquipmentTab.js
@@ -1,23 +1,21 @@
-import React,{ useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React,{ useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import Equipment from '../CaseFiles/OverlayPages/ChargeSheet/Equipment';
-import Table from "../common/Table/Table";
-import Footer from "../common/Page/Footer";
+import Table from '../common/Table/Table';
+import Footer from '../common/Page/Footer';
 import { currencyFormatter } from '../../utils/formatter';
-import LongPressWithFeedback from "../common/LongPressWithFeedback";
-import ActionContainer from "../common/FloatingAction/ActionContainer";
-import ActionItem from "../common/ActionItem";
+import LongPressWithFeedback from '../common/LongPressWithFeedback';
+import ActionContainer from '../common/FloatingAction/ActionContainer';
+import ActionItem from '../common/ActionItem';
 import {LONG_PRESS_TIMER} from '../../const';
 import { useModal } from 'react-native-modalfy';
 import styled, { css } from '@emotion/native';
 import { useTheme } from 'emotion-theming';
-import WasteIcon from "../../../assets/svg/wasteIcon";
-import TransferIcon from "../../../assets/svg/transferIcon";
+import WasteIcon from '../../../assets/svg/wasteIcon';
+import TransferIcon from '../../../assets/svg/transferIcon';
 import Item from '../common/Table/Item';
 import DataItem from '../common/List/DataItem';
 import {useNextPaginator, usePreviousPaginator, checkboxItemPress, selectAll} from '../../helpers/caseFilesHelpers';
-
-
 
 const StorageEquipmentTab = ({equipments = []}) => {
     const theme = useTheme();
@@ -25,73 +23,74 @@ const StorageEquipmentTab = ({equipments = []}) => {
 
     const headers = [
         {
-            name :"Item Name",
-            alignment: "flex-start"
+            name: 'Item Name',
+            alignment: 'flex-start'
         },
         {
-            name :"Category",
-            alignment: "center"
+            name: 'Category',
+            alignment: 'center'
         },
         {
-            name :"On-Hand",
-            alignment: "center"
+            name: 'On-Hand',
+            alignment: 'center'
         },
         {
-            name :"Unit Price",
-            alignment: "flex-end"
+            name: 'Unit Price',
+            alignment: 'flex-end'
         }
-    ]
+    ];
 
     const [checkedItems, setCheckedItems] = useState([]);
     const [isFloatingDisabled, setFloatingAction] = useState(false);
 
-    const listItem = (item) =>
+    const listItem = item => (
         <>
-            <DataItem flex = {2} color = "--color-blue-600" fontStyle = "--text-base-medium" text = {item?.name}/>
-            <DataItem align = "center" color = "--color-gray-600" fontStyle = "--text-base-regular" text = {item?.type}/>
-            <DataItem align = "center" color = "--color-gray-600" fontStyle = "--text-base-regular" text = {item?.amount}/>
-            <DataItem align = "flex-end" color = "--color-gray-600" fontStyle = "--text-base-regular" text = {item.unitPrice}/>
+            <DataItem flex={2} color="--color-blue-600" fontStyle="--text-base-medium" text={item?.name || ''}/>
+            <DataItem align="center" color="--color-gray-600" fontStyle="--text-base-regular" text={item?.type || ''}/>
+            <DataItem align="center" color="--color-gray-600" fontStyle="--text-base-regular" text={item?.amount || 0}/>
+            <DataItem align="flex-end" color="--color-gray-600" fontStyle="--text-base-regular" text={item.unitPrice || 0}/>
+        </>
+    );
 
-        </>;
+    const renderListItem = item => {
+        return (
+            <Item
+                itemView={listItem(item)}
+                hasCheckBox={true}
+                isChecked={checkedItems.includes(item?._id)}
+                onCheckBoxPress={() => onItemCheck(item)}
+                onItemPress={() => {}}
+            />
+        );
+    };
 
-    const renderListItem = (item) => {
-        <Item
-            itemView = {listItem(item)}
-            hasCheckBox = {true}
-            isChecked = {checkedItems.includes(_id)}
-            onCheckBoxPress = {()=>onItemCheck(item)}
-            onItemPress = {()=>{}}
-        />
-    }
+    const handleHeaderCheckbox = () => {
+        let updatedItemsList = selectAll(equipments, checkedItems);
+        setCheckedItems(updatedItemsList);
+    };
 
-    const handleHeaderCheckbox = () =>{
-        let updatedItemsList = selectAll(consumables, checkedItems);
-        setCheckedItems(updatedItemsList)
-    }
-
-    const onItemCheck = (item) =>{
+    const onItemCheck = (item) => {
         const { _id } = item;
         let updatedItems = checkboxItemPress(_id, checkedItems);
-        setCheckedItems(updatedItems)
-
-    }
+        setCheckedItems(updatedItems);
+    };
 
     const toggleActionButton = () => {
-        setFloatingAction(true)
+        setFloatingAction(true);
 
-        modal.openModal("ActionContainerModal",
+        modal.openModal('ActionContainerModal',
             {
                 actions: floatingActions(),
-                title: "SUPPLIER ACTIONS",
+                title: 'SUPPLIER ACTIONS',
                 onClose: () => {
-                    setFloatingAction(false)
+                    setFloatingAction(false);
                 },
-            })
-    }
+            });
+    };
 
-    const floatingActions = () =>{
-        let isDisabled = checkedItems.length === 0 ? true : false;
-        let isDisabledColor = checkedItems.length === 0 ? theme.colors['--color-gray-600'] : theme.colors['--color-red-700']
+    const floatingActions = () => {
+        let isDisabled = checkedItems.length === 0;
+        let isDisabledColor = checkedItems.length === 0 ? theme.colors['--color-gray-600'] : theme.colors['--color-red-700'];
         // const deleteItem =
         //     <LongPressWithFeedback
         //         pressTimer={LONG_PRESS_TIMER.MEDIUM}
@@ -107,38 +106,39 @@ const StorageEquipmentTab = ({equipments = []}) => {
         //         />
         //     </LongPressWithFeedback>;
 
-        const itemTransfer =
+        const itemTransfer = (
             <ActionItem
-                title={"Item Transfer"}
-                icon={<TransferIcon strokeColor = {isDisabled ? theme.colors['--color-gray-600'] : theme.colors['--color-orange-700']}/>}
+                title="Item Transfer"
+                icon={<TransferIcon strokeColor={isDisabled ? theme.colors['--color-gray-600'] : theme.colors['--color-orange-700']}/>}
                 onPress={() => handleTransferItems()}
-                disabled = {isDisabled}
+                disabled={isDisabled}
                 touchable={!isDisabled}
             />
+        );
 
         return <ActionContainer
             floatingActions={[
                 itemTransfer
             ]}
-            title={"SUPPLIER ACTIONS"}
-        />
-    }
+            title="SUPPLIER ACTIONS"
+        />;
+    };
 
-    const handleTransferItems = () => {}
+    const handleTransferItems = () => {};
 
     return (
         <>
             <Table
-                data = {equipments}
-                listItemFormat = {renderListItem}
-                headers = {headers}
-                isCheckbox = {true}
-                toggleHeaderCheckbox = {handleHeaderCheckbox}
-                itemSelected = {checkedItems}
+                data={equipments}
+                listItemFormat={renderListItem}
+                headers={headers}
+                isCheckbox={true}
+                toggleHeaderCheckbox={handleHeaderCheckbox}
+                itemSelected={checkedItems}
             />
 
             <Footer
-                toggleActionButton = {toggleActionButton}
+                toggleActionButton={toggleActionButton}
             />
         </>
         // <Equipment
@@ -147,17 +147,17 @@ const StorageEquipmentTab = ({equipments = []}) => {
         //     listItemFormat = {listItem}
         //     toggleActionButton = {toggleActionButton}
         // />
-    )
-}
+    );
+};
 
-export default StorageEquipmentTab
+export default StorageEquipmentTab;
 
 const styles = StyleSheet.create({
-    item:{
-        flex:1,
+    item: {
+        flex: 1,
     },
-    itemText:{
-        fontSize:16,
-        color:"#4A5568",
+    itemText: {
+        fontSize: 16,
+        color: '#4A5568',
     },
-})
+});
