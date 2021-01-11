dacc33f3c35f904f03e603212f181bd4089b97cb
Danielle Scott <danielle.scott@smsja.net>
2021-01-11 17:18:04 -0500

    --created page for supplier invoice details tab and added to stack.

--- src/components/navigation/AppStack/SupplierNavigationStack.js
+++ src/components/navigation/AppStack/SupplierNavigationStack.js
@@ -1,18 +1,17 @@
-import React from "react";
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Suppliers from "../../../page/Suppliers";
-import SupplierPage from "../../../page/Suppliers/SupplierPage";
-import NotFound from "../../../page/NotFound";
-import HeaderBackComponent from "../components/HeaderBackComponent";
-import ArchivedSuppliers from "../../../page/ArchivedSuppliers/ArchivedSuppliersPage";
-import ArchivedSupplier from "../../../page/ArchivedSuppliers/ArchivedSupplier";
-import SupplierProductPage from "../../../page/Suppliers/SupplierProductPage";
-import SupplierProductCreationPage from "../../../page/Suppliers/SupplierProductCreationPage";
-
+import Suppliers from '../../../page/Suppliers';
+import SupplierPage from '../../../page/Suppliers/SupplierPage';
+import SupplierInvoiceUpload from '../../../page/Suppliers/SupplierInvoiceUpload';
+import NotFound from '../../../page/NotFound';
+import HeaderBackComponent from '../components/HeaderBackComponent';
+import ArchivedSuppliers from '../../../page/ArchivedSuppliers/ArchivedSuppliersPage';
+import ArchivedSupplier from '../../../page/ArchivedSuppliers/ArchivedSupplier';
+import SupplierProductPage from '../../../page/Suppliers/SupplierProductPage';
+import SupplierProductCreationPage from '../../../page/Suppliers/SupplierProductCreationPage';
 
 const Stack = createStackNavigator();
-
 
 export default (props) => {
     const isAdmin = props.route.params.isAdmin || false;
@@ -45,7 +44,7 @@ export default (props) => {
                         height: 100,
 
                     },
-                    headerTitle: ""
+                    headerTitle: ''
                 }}
             />
 
@@ -63,7 +62,7 @@ export default (props) => {
                         height: 100,
 
                     },
-                    headerTitle: ""
+                    headerTitle: ''
                 }}
             />
 
@@ -81,10 +80,9 @@ export default (props) => {
                         height: 100,
 
                     },
-                    headerTitle: ""
+                    headerTitle: ''
                 }}
             />
-
 
             <Stack.Screen
                 name="ArchivedSuppliers"
@@ -100,7 +98,7 @@ export default (props) => {
                         height: 100,
 
                     },
-                    headerTitle: ""
+                    headerTitle: ''
                 }}
             />
             <Stack.Screen
@@ -117,10 +115,28 @@ export default (props) => {
                         height: 100,
 
                     },
-                    headerTitle: ""
+                    headerTitle: ''
+                }}
+            />
+
+            <Stack.Screen
+                name="SupplierInvoiceUpload"
+                component={SupplierInvoiceUpload}
+                options={{
+                    headerShown: false,
+                    headerLeft: (props) => (
+                        <HeaderBackComponent
+                            {...props}
+                        />
+                    ),
+                    headerStyle: {
+                        height: 100,
+
+                    },
+                    headerTitle: ''
                 }}
             />
 
         </Stack.Navigator>
     );
-}
+};
---
+++ src/page/Suppliers/SupplierInvoiceUpload.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const SupplierInvoiceUpload = () => {
+    return (
+        <View/>
+    );
+};
+
+export default SupplierInvoiceUpload;
