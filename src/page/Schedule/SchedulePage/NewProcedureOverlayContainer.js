40e5efb524511e84333b3cfda10ce3d027ce681c
Donovan Saunches <donovan.saunches@smsja.net>
2022-11-04 15:57:02 -0500

    overlay container fix

--- src/page/Schedule/SchedulePage/NewProcedureOverlayContainer.js
+++ src/page/Schedule/SchedulePage/NewProcedureOverlayContainer.js
@@ -343,30 +343,8 @@ function NewProcedureOverlayContainer({ appointment = {}, editMode = false, pass
 
 
 
-    } 
-
-    const handleConfirm =()=>{
-        console.log("just a test",errors)
-                modal.openModal('ConfirmationModal',
-                    {
-                        content: <ConfirmationComponent
-                            isError={false}
-                            isEditUpdate={false}
-                            onCancel={() => {
-                                modal.closeModals('ConfirmationModal');
-                            }}
-                            onAction={() => {
-                                modal.closeAllModals();
-
-                            }}
-                            message="Completed Successfully!"
-                        // onAction = { () => confirmAction()}
-                        />,
-                        onClose: () => {
-                            modal.closeModals('ConfirmationModal');
-                        }
-                    });
-    }
+        }
+    }, [allowedToSubmit])
 
 
     const hanadleErrorModal = () => {
