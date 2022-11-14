c8fc28845a9763a642b462abe7839ea64a22de42
Shantel Wellington <swellington@SMS.LOCAL>
2022-11-14 14:49:57 -0500

    added storage table

--- src/components/Settings/StoragePage.js
+++ src/components/Settings/StoragePage.js
@@ -3,19 +3,26 @@ import React, {useState, useEffect, useContext} from 'react';
 import {View} from 'react-native';
 import styled, {css} from '@emotion/native';
 import { useTheme } from 'emotion-theming';
+import FrameCard from '../common/Frames/FrameCards/FrameCard';
 
-
-
-function StoragePage({navigation}) {
+const StoragePage = () => {
+    return (
+        <View style={styles.frameContainer} key={index}>
+            <FrameLifestyleCard
+                frameColor = "#4E5664"
+                titleBackgroundColor = "#EEF2F6"
+                frameBorderColor = "#A0AEC0"
+                frameTitle = "Categories"              
+            />
+        </View>
+    )
 
 }
 
-
-
-
-
-
-
-
+const styles = StyleSheet.create({
+    frameContainer:{
+        marginBottom:20
+    }
+})
 
 export default StoragePage;
