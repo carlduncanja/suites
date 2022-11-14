c5342e7cb829364e33ecbef143b1e44e59542894
Shantel Wellington <swellington@SMS.LOCAL>
2022-11-14 15:06:25 -0500

    edited storage page

--- src/components/Settings/StoragePage.js
+++ src/components/Settings/StoragePage.js
@@ -1,28 +1,28 @@
 
-import React, {useState, useEffect, useContext} from 'react';
-import {View} from 'react-native';
-import styled, {css} from '@emotion/native';
-import { useTheme } from 'emotion-theming';
-import FrameCard from '../common/Frames/FrameCards/FrameCard';
+// import React, {useState, useEffect, useContext} from 'react';
+// import {View} from 'react-native';
+// import styled, {css} from '@emotion/native';
+// import { useTheme } from 'emotion-theming';
+// import FrameCard from '../common/Frames/FrameCards/FrameCard';
 
-const StoragePage = () => {
-    return (
-        <View style={styles.frameContainer} key={index}>
-            <FrameLifestyleCard
-                frameColor = "#4E5664"
-                titleBackgroundColor = "#EEF2F6"
-                frameBorderColor = "#A0AEC0"
-                frameTitle = "Categories"              
-            />
-        </View>
-    )
+// const StoragePage = () => {
+//     return (
+//         <View style={styles.frameContainer} key={index}>
+//             <FrameLifestyleCard
+//                 frameColor = "#4E5664"
+//                 titleBackgroundColor = "#EEF2F6"
+//                 frameBorderColor = "#A0AEC0"
+//                 frameTitle = "Categories"              
+//             />
+//         </View>
+//     )
 
-}
+// }
 
-const styles = StyleSheet.create({
-    frameContainer:{
-        marginBottom:20
-    }
-})
+// const styles = StyleSheet.create({
+//     frameContainer:{
+//         marginBottom:20
+//     }
+// })
 
-export default StoragePage;
+// export default StoragePage;
