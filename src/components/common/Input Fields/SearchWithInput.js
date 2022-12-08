e0be2f65fbcb063e9119f245ac1c958da62dd006
Donovan Saunches <donovan.saunches@smsja.net>
2022-12-08 12:48:55 -0500

    code cleanup -qa "Ref 909"

--- src/components/common/Input Fields/SearchWithInput.js
+++ src/components/common/Input Fields/SearchWithInput.js
@@ -69,15 +69,6 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "space-between",
     }
-    // bigBlue: {
-    //   color: 'blue',
-    //   fontWeight: 'bold',
-    //   fontSize: 30,
-    // },
-
-    // red: {
-    //   color: 'red',
-    // },
   });
 
 export default SearchWithInput;
