e65f646575f96a4d86f1b3fed9398d2d807c92ec
Donovan Saunches <donovan.saunches@smsja.net>
2022-12-12 11:29:50 -0500

    action menu & page footer "Ref #991"

--- src/components/OverlayTabs/PaymentHistoryTab.js
+++ src/components/OverlayTabs/PaymentHistoryTab.js
@@ -1,27 +1,14 @@
 import React, { useRef, useContext, useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import Record from "../common/Information Record/Record";
-import Row from "../common/Row";
-import ResponsiveRecord from "../common/Information Record/ResponsiveRecord";
-import { formatDate, formatToCurrency, handleNumberValidation } from "../../utils/formatter";
-import { formatAmount } from "../../helpers/caseFilesHelpers";
-import { transformToSentence } from "../../hooks/useTextEditHook";
-import DateInputField from '../common/Input Fields/DateInputField';
-import LineDivider from "../common/LineDivider";
 import styled, { css } from '@emotion/native';
 import { useTheme } from 'emotion-theming';
 import { PageContext } from "../../contexts/PageContext";
-import ConfirmationComponent from '../ConfirmationComponent';
-import { getStorage, updatePurchaseOrder, updatePurchaseOrderDetails } from '../../api/network';
 import { useModal } from "react-native-modalfy";
-import FieldContainer from "../common/FieldContainerComponent";
-import InputWrapper from "../common/Input Fields/InputWrapper";
-import InputLabelComponent from "../common/InputLablel";
-import InputField2 from "../common/Input Fields/InputField2";
-import SearchableOptionsField from "../common/Input Fields/SearchableOptionsField";
-import _ from "lodash";
-import Footer from "../common/Page/Footer";
 import EmptyPaymentHistoryContainer from "../PurchaseOrders/EmptyPaymentHistoryContainer";
+import AddIcon from "../../../assets/svg/addIcon";
+import Footer from "../common/Page/Footer";
+import ActionItem from "../common/ActionItem";
+import ActionContainer from "../common/FloatingAction/ActionContainer";
 
 const LineDividerContainer = styled.View`
     margin-bottom : ${({ theme }) => theme.space['--space-32']};
@@ -29,7 +16,7 @@ const LineDividerContainer = styled.View`
 
 
 const PaymentHistoryTab = ({
-
+    order,
 }) => {
 
     const theme = useTheme();
@@ -37,10 +24,64 @@ const PaymentHistoryTab = ({
     const baseStateRef = useRef();
     const { pageState, setPageState } = useContext(PageContext);
     const { isEditMode } = pageState;
+    const [selectedPayment, setSelectedPayment] = useState();
+    const [isFloatingActionDisabled, setFloatingAction] = useState(false);
 
-  
+    const { payments } = order;
+
+    const floatingActions = () => {
+
+        const addItem = (
+            <ActionItem
+                title="Register Payment"
+                icon={<AddIcon strokeColor={selectedPayment ? theme.colors['--color-green-700'] : theme.colors['--color-gray-600']} />}
+                onPress={() => { console.log("TBA") }}
+                disabled={!selectedPayment}
+                touchable={selectedPayment}
+            />
+        );
+
+
+        return <ActionContainer
+            floatingActions={[
+                addItem,
+            ]}
+            title="PAYMENT ACTIONS"
+        />;
+    };
+
+    const toggleActionButton = () => {
+        setFloatingAction(true);
+
+        modal.openModal('ActionContainerModal',
+            {
+                actions: floatingActions(),
+                title: 'ORDER ACTIONS',
+                onClose: () => {
+                    setFloatingAction(false);
+                },
+            });
+    };
+
+
     return (
-     <EmptyPaymentHistoryContainer></EmptyPaymentHistoryContainer>
+        <>
+            {
+                payments?.length ?
+
+                    <></>
+                    :
+                    <EmptyPaymentHistoryContainer />
+            }
+
+
+            <Footer
+                hasPaginator={false}
+                isDisabled={isFloatingActionDisabled}
+                toggleActionButton={toggleActionButton}
+            />
+
+        </>
 
     )
 }
--- src/page/PurchaseOrders/OrderItemPage.js
+++ src/page/PurchaseOrders/OrderItemPage.js
@@ -322,7 +322,7 @@ function OrderItemPage({ route, navigation }) {
             case 'Invoice':
                 return <RequisitionTab key={2} order={selectedOrder} onUpdate={() => fetchOrder(_id)} type={ORDER_TYPES.PURCHASE_ORDER}/>;
             case 'Payments':
-                return <PaymentHistoryTab/>
+                return <PaymentHistoryTab order={selectedOrder} />
             default:
                 return <View />;
         }
