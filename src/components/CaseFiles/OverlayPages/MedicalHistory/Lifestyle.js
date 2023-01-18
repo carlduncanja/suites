9836ff3e8d6570fb6c1940e156b23e48aea06184
Dean Morgan <deanmorgan@Dean-MacBook.local>
2023-01-18 17:03:25 -0500

    edit mode switch isa design redo

--- src/components/CaseFiles/OverlayPages/MedicalHistory/Lifestyle.js
+++ src/components/CaseFiles/OverlayPages/MedicalHistory/Lifestyle.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Picker, ScrollView, StyleSheet } from "react-native";
+import { View, Picker, ScrollView, StyleSheet, Text } from "react-native";
 import { DrugUse, AlcoholUse, TobaccoUse } from '../../OverlayCardFrames'
 
 import TobaccoIcon from '../../../../../assets/svg/tobacco';
@@ -7,12 +7,15 @@ import DrugIcon from '../../../../../assets/svg/drug';
 import AlcoholIcon from '../../../../../assets/svg/alcohol';
 
 import FrameLifestyleCard from '../../../common/Frames/FrameCards/FrameLifestyleCard'
+import DisabledSectionComponent from '../../../DisabledSectionComponent';
+import FrameTitle from '../../../common/Frames/FrameTitle';
+const Lifestyle = ({ tabDetails, isEditMode}) => {
 
-const Lifestyle = ({tabDetails}) => {
 
-    const getTypeData = (lifestyleType) =>{
-        return tabDetails.filter( item => {
-            const { type = {name:"Molly"} } = item
+
+    const getTypeData = (lifestyleType) => {
+        return tabDetails.filter(item => {
+            const { type = { name: "Molly" } } = item
             const { name = "Drug Use" } = type
             return name === lifestyleType && item
         }) || []
@@ -21,60 +24,75 @@ const Lifestyle = ({tabDetails}) => {
     const drugs = getTypeData("Drug Use")
     const alcohol = getTypeData("Alcohol Use")
     const tobacco = getTypeData("Tobacco Use")
-    
-    return ( 
-        <ScrollView>
-            {drugs.map((drug, index)=>{
-                return (
-                    <View style={styles.frameContainer} key={index}>
-                        <FrameLifestyleCard
-                            frameColor = "#805AD5"
-                            titleBackgroundColor = "#FAF5FF"
-                            frameBorderColor = "#D6BCFA"
-                            frameTitle = "Drug Use"
-                            cardInformation = {drug}
-                            icon = {DrugIcon}
-                        />
-                    </View>
-                )
-            })}
 
-            {alcohol.map((alcoholItem, index)=>{
-                return (
-                    <View style={styles.frameContainer} key={index}>
-                        <FrameLifestyleCard
-                            frameColor = "#E53E3E"
-                            titleBackgroundColor = "#FFF5F5"
-                            frameBorderColor = "#FEB2B2"
-                            frameTitle = "Alcohol Use"
-                            cardInformation = {alcoholItem}
-                            icon = {AlcoholIcon}
-                        />
-                    </View>
-                )
-            })}
-            {tobacco.map((tobaccoItem, index)=>{
-                return (
-                    <View style={styles.frameContainer} key={index}>
-                        <FrameLifestyleCard
-                            frameColor = "#4E5664"
-                            titleBackgroundColor = "#EEF2F6"
-                            frameBorderColor = "#A0AEC0"
-                            frameTitle = "Tobacco Use"
-                            cardInformation = {tobaccoItem}
-                            icon = {TobaccoIcon}
-                        />
-                    </View>
-                )
-            })}
-        </ScrollView>
+    return (
+        tabDetails.length != 0 ?
+            <ScrollView>
+                {drugs.length != 0 ?
+                    
+                        <View style={styles.frameContainer} >
+                            <View style={styles.container}>
+                                
+                                <FrameLifestyleCard
+                                    frameColor="#805AD5"
+                                    titleBackgroundColor="#FAF5FF"
+                                    frameBorderColor="#D6BCFA"
+                                    frameTitle="Drug Use"
+                                    cardInformation={drugs}
+                                    icon={DrugIcon}
+                                    isEditMode={isEditMode}
+                                />
+                            </View> 
+
+                        </View>
+                    :
+                    null
+                }
+
+                {alcohol.map((alcoholItem, index) => {
+                    return (
+                        <View style={styles.frameContainer} key={index}>
+                            <FrameLifestyleCard
+                                frameColor="#E53E3E"
+                                titleBackgroundColor="#FFF5F5"
+                                frameBorderColor="#FEB2B2"
+                                frameTitle="Alcohol Use"
+                                cardInformation={alcoholItem}
+                                icon={AlcoholIcon}
+                            />
+                        </View>
+                    )
+                })}
+                {tobacco.map((tobaccoItem, index) => {
+                    return (
+                        <View style={styles.frameContainer} key={index}>
+                            <FrameLifestyleCard
+                                frameColor="#4E5664"
+                                titleBackgroundColor="#EEF2F6"
+                                frameBorderColor="#A0AEC0"
+                                frameTitle="Tobacco Use"
+                                cardInformation={tobaccoItem}
+                                icon={TobaccoIcon}
+                            />
+                        </View>
+                    )
+                })}
+            </ScrollView>
+            :
+
+            <DisabledSectionComponent />
+
+
     );
 }
- 
+
 export default Lifestyle;
 
 const styles = StyleSheet.create({
-    frameContainer:{
-        marginBottom:20
-    }
+    frameContainer: {
+        marginBottom: 20
+    },
+    container: {
+        backgroundColor: '#F8FAFB',
+    },
 })
--- src/components/common/Frames/FrameCards/FrameLifestyleCard.js
+++ src/components/common/Frames/FrameCards/FrameLifestyleCard.js
@@ -1,52 +1,81 @@
-import React from 'react';
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState} from 'react';
+import { View, Text, StyleSheet, } from "react-native";
 import FrameTitle from '../FrameTitle';
 import FrameLifestyleContent from '../FrameContents/FrameLifestyleContent';
-import {transformToSentence} from '../../../../hooks/useTextEditHook'
+import FrameItem from '../FrameItems/FrameItem';
+import { transformToSentence } from '../../../../hooks/useTextEditHook'
+import { ScrollView } from 'react-native-gesture-handler';
+import AddIcon from '../../../../../assets/svg/addIcon';
 
-const FrameLifestyleCard = (props) => { 
+const FrameLifestyleCard = (props) => {
+    console.log(props.cardInformation)
+    
+    const [addMode, setAddMode] = useState(false) 
+
+    const toggleAddOption = (value) => {
+        setAddMode(value)
+    }
     return (
         <View style={styles.container}>
-                <View style={styles.title}>
-                    <FrameTitle
-                        color={props.frameColor}
-                        borderColor = {props.frameBorderColor}
-                        backgroundColor={props.titleBackgroundColor}
-                        icon={props.icon}
-                        frameTitle={props.frameTitle}
-                    />
-                </View>
-                <View style={styles.content}>
-                    <Text style={styles.titleName}>{transformToSentence(props.cardInformation.name)}</Text>
-                    <FrameLifestyleContent cardInformation={props.cardInformation}/>
-                </View>
+            <View style={styles.title}>
+                <FrameTitle
+                    color={props.frameColor}
+                    borderColor={props.frameBorderColor}
+                    backgroundColor={props.titleBackgroundColor}
+                    icon={props.icon}
+                    frameTitle={props.frameTitle}
+                />
             </View>
+            <View style={styles.content} >
+                {props.cardInformation.map((categorieInformation, index) => {
+                    return (
+                        <View>
+                            <Text style={styles.titleName}>{transformToSentence(categorieInformation.name)}</Text>
+                            <FrameLifestyleContent cardInformation={categorieInformation} />
+                        </View>
+                    )
+                })}
+                {props.isEditMode ?
+
+                    addMode ?
+                        null
+                        :
+                        <View>
+                            <FrameItem itemContent="Add New item" icon={<AddIcon />} isEditMode={props.isEditMode} onPressButton={() => { toggleAddOption(true) }} />
+                        </View>
+                    :
+                    null
+
+                }
+            </View>
+        </View>
     );
 }
 
 export default FrameLifestyleCard;
 
 const styles = StyleSheet.create({
-    container:{
-        backgroundColor:'#F8FAFB',
+    container: {
+        backgroundColor: '#F8FAFB',
     },
-    title:{
-        width:'100%'
+    title: {
+        width: '100%'
     },
-    content:{
-        width:'100%',
-        padding:15,
-        borderWidth:1,
-        borderColor:'#CCD6E0',
-        borderTopWidth:0,
-        borderBottomLeftRadius:8,
-        borderBottomRightRadius:8
+    content: {
+        width: '100%',
+        padding: 15,
+        borderWidth: 1,
+        borderColor: '#CCD6E0',
+        borderTopWidth: 0,
+        borderBottomLeftRadius: 8,
+        borderBottomRightRadius: 8,
+
 
     },
-    titleName:{
-        marginLeft:2,
-        color:"#4E5664",
-        fontSize:16,
+    titleName: {
+        marginLeft: 2,
+        color: "#4E5664",
+        fontSize: 16,
         fontWeight: 'bold'
     }
 })
