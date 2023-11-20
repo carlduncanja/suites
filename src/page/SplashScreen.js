2cb57dfad1f9f383d87d0a703a701475eb599cf8
shantelSMS <119514794+shantelSMS@users.noreply.github.com>
2023-11-20 16:13:56 -0500

    removed unnecessary functions

--- src/page/SplashScreen.js
+++ src/page/SplashScreen.js
@@ -1,32 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { View, StyleSheet, ActivityIndicator } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import LoginBackground from "../components/Onboarding/LoginBackground";
 import { restoreToken } from "../redux/actions/authActions";
 import { connect } from "react-redux";
-import { setBearerToken } from "../api";
 
-function SplashScreen({ restoreToken }) {
-    useEffect(() => {
-        const bootstrapAsync = async () => {
-            let userToken;
-
-            try {
-                userToken = await AsyncStorage.getItem("userToken");
-            } catch (e) {
-                console.log("failed to get tokens");
-            }
-
-            if (userToken) {
-                setBearerToken(userToken);
-            }
-
-            restoreToken(userToken);
-        };
-
-        bootstrapAsync().then();
-    }, []);
-
+function SplashScreen() {
     return (
         <View style={styles.container}>
             <View style={styles.background}>
